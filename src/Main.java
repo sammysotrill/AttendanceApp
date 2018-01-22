c4986ea288499c111213e7342ba7fa3c0cd1be76
Samuel Maxey <smaxey@xula.edu>
2018-01-22 10:51:47 -0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static int main(String[] args) {
 
         // Create welcome message
         System.out.println("\n\nHello , AttendanceApp! \n");
@@ -16,12 +16,44 @@ public class Main {
         //Create an output list of absences
         ArrayList<Integer> absences = new ArrayList<Integer>();
         Random rand = new Random();
-        for (int i = 0; i < name.length() ; i++) {
+        for (int i = 0; i < name.length(); i++) {
             absences.add(rand.nextInt(11));
-
 
 
         }
         System.out.println("The elements are: " + absences);
+
+        //Number of people with perfect attendance
+        int count = 0;
+        for (int i = 0; i < name.length(); i++) {
+            if (absences.get(i) == 0) {
+                count = count++;
+
+            }
+
+        }
+        return count;
+
+        // Calculate average of absences
+        double avg = average(absences);
+        System.out.println("The average of absences is : " + avg);
+
+
+
+    }
+    private static int sum (ArrayList<Integer> absences){
+        int sum = 0;
+        for (int i = 0; i < absences.size() ; i++) {
+            sum = sum + absences.get(i);
+
+        }
+        return sum;
+
+    }
+
+    private static double average(ArrayList<Integer> absences){
+        return (double)sum(absences)/absences.size();
+
+
     }
 }
