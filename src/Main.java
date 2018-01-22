0e46a8ddb98688315f530444f993b551e5fddd54
Samuel Maxey <smaxey@xula.edu>
2018-01-22 10:28:43 -0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // Create welcome message
         System.out.println("\n\nHello , AttendanceApp! \n");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = sc.nextLine();
         System.out.println("Welcome, " + name + "!");
+
+        //Create an output list of absences
+        ArrayList<Integer> absences = new ArrayList<Integer>();
+        Random rand = new Random();
+        for (int i = 0; i < name.length() ; i++) {
+            absences.add(rand.nextInt(11));
+
+
+
+        }
+        System.out.println("The elements are: " + absences);
     }
 }
