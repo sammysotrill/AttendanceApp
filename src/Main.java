d8312a82b4403a0936b99424595e7bb56ef82be5
Samuel Maxey <smaxey@xula.edu>
2018-01-22 22:42:00 -0600

    in business

--- src/Main.java
+++ src/Main.java
@@ -4,55 +4,56 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int main(String[] args) {
+    public static void main(String[] args) {
+        // Add a welcome message to the attendance app
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = scan.nextLine();
+        System.out.println("==========Hello " + name + " Welcome to Our Attendance App=============  \n");
 
-        // Create welcome message
-        System.out.println("\n\nHello , AttendanceApp! \n");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String name = sc.nextLine();
-        System.out.println("Welcome, " + name + "!");
-
-        //Create an output list of absences
+        //Create and then output the list of absences.
         ArrayList<Integer> absences = new ArrayList<Integer>();
         Random rand = new Random();
         for (int i = 0; i < name.length(); i++) {
             absences.add(rand.nextInt(11));
 
-
         }
-        System.out.println("The elements are: " + absences);
+        System.out.println("The list of absences " + absences);
 
-        //Number of people with perfect attendance
+        //How many students had perfect attendance?
+        System.out.println("There are " + perfAtt(absences) + " students with perfect attendance.");
+
+        //The average of the absences
+        System.out.println("The average of all the absences is: " + avg(absences, sum(absences)));
+    }
+
+
+    public static int perfAtt(ArrayList<Integer> list) {
         int count = 0;
-        for (int i = 0; i < name.length(); i++) {
-            if (absences.get(i) == 0) {
-                count = count++;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == 0) {
+                count = count + 1;
 
             }
 
+
         }
         return count;
-
-        // Calculate average of absences
-        double avg = average(absences);
-        System.out.println("The average of absences is : " + avg);
-
-
-
     }
-    private static int sum (ArrayList<Integer> absences){
+
+    public static int sum(ArrayList<Integer> list) {
         int sum = 0;
-        for (int i = 0; i < absences.size() ; i++) {
-            sum = sum + absences.get(i);
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + list.get(i);
 
         }
         return sum;
-
     }
 
-    private static double average(ArrayList<Integer> absences){
-        return (double)sum(absences)/absences.size();
+    public static double avg(ArrayList<Integer> list, int sum) {
+        double avg = sum / list.size();
+
+        return avg;
 
 
     }
