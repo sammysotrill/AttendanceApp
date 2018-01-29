fd81b8154907cab4372c0f7af254b9c27d75a7ff
Samuel Maxey <smaxey@xula.edu>
2018-01-29 14:45:52 -0600

    in business

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -27,29 +30,88 @@ public class Main {
         System.out.println("The average of all the absences is: " + avg(absences, sum(absences)));
 
         //Percentage of students who had fewer than 3 absences also had perfect attendance
-        System.out.println("The Percentage of students who had fewer than 3 absences also had perfect attendance is: " + prcnt(absences) + "%");
+        //System.out.println("The Percentage of students who had fewer than 3 absences also had perfect attendance is: " + prcnt(absences) + "%");
 
         //Which students had [X] absences
-        ArrayList<Integer>students = students(absences,8);
+        ArrayList<Integer> students = students(absences, 8);
         System.out.println("These students had your indicated absences: " + students);
 
         //Which and what percentage of the students have FE'd the course
-        ArrayList<Integer>FEs = FEs(absences, 2);
+        ArrayList<Integer> FEs = FEs(absences, 2);
         System.out.println("These students have Fe'd the course " + FEs);
 
         //Add x to any absences greater than y
-        ArrayList<Integer> plusList = plusList(absences,3,4);
+        ArrayList<Integer> plusList = plusList(absences, 3, 4);
         System.out.println("The list with added absences: " + plusList);
 
+        // Sort the absences using a library function
+        Collections.sort(absences);
+        System.out.println("The sorted list of absences: " + absences);
 
+        //Shuffle the absences using a library function
+        Collections.shuffle(absences);
+        System.out.println("The shuffled list of absences: " + absences);
 
+        //How many of the absences are unique?
+        uniAbs(absences);
+
+        //How many of each absence value are there?
+        countUnq(absences);
+
+        // Sort the absences using a user - defined sort function
+        bubbleSort(absences);
+        System.out.println("The new sorted list: " + absences);
+
+        //Sort the absences using a user - defined shuffle function
+        bubbleShuffle(absences);
+        System.out.println("The new shuffled list: " + absences);
+
+        // Create and output an ArrayList of 5 distinct names
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Samuel");
+        names.add("Chandler");
+        names.add("Chibeza");
+        names.add("Jenaro");
+        names.add("Myles");
+        System.out.println("The names in the list are: " + names);
+;
 
     }
 
-    private static ArrayList<Integer> plusList(ArrayList<Integer> absences,int addend, int DaysMissed) {
-        ArrayList<Integer>list = new ArrayList<Integer>();
+    private static void bubbleShuffle(ArrayList<Integer> absences) {
+            Random rand = new Random();
+            for (int i = 0; i < absences.size(); i++) {
+                absences.set(rand.nextInt(absences.size()),i);
+
+
+        }
+    }
+
+    private static void bubbleSort(ArrayList<Integer> absences) {
         for (int i = 0; i < absences.size() ; i++) {
-            if(absences.get(i) > DaysMissed){
+            for (int j = 0; j < absences.size() ; j++) {
+                if(absences.get(i) < absences.get(j)){
+                    int temp = absences.get(i);
+                    absences.set(i,absences.get(j));
+                    absences.set(j,temp);
+                }
+
+            }
+
+        }
+    }
+
+    private static void uniAbs(ArrayList<Integer> absences) {
+    }
+
+    private static void countUnq(ArrayList<Integer> absences) {
+
+    }
+
+    private static ArrayList<Integer> plusList(ArrayList<Integer> absences, int addend, int DaysMissed) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < absences.size(); i++) {
+            if (absences.get(i) > DaysMissed) {
                 list.add(absences.get(i) + addend);
             }
 
@@ -89,7 +151,7 @@ public class Main {
 
     }
 
-    public static double prcnt(ArrayList<Integer> list) {
+    /*public static double prcnt(ArrayList<Integer> list) {
         int count1 = 0;
         int count2 = 0;
         double pct = 0;
@@ -105,31 +167,27 @@ public class Main {
             pct = pct + (count2 / count1) * 100;
 
         }
-        return pct;
+        return pct;*/
 
 
-    }
-
-    public static ArrayList<Integer> students(ArrayList<Integer>absences, int NumOfMissedDays){
-        ArrayList<Integer>list = new ArrayList<Integer>();
+    public static ArrayList<Integer> students(ArrayList<Integer> absences, int NumOfMissedDays) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i = 0; i < absences.size(); i++) {
-            if(absences.get(i) == NumOfMissedDays){
+            if (absences.get(i) >= NumOfMissedDays) {
                 list.add(i);
             }
-
 
 
         }
         return list;
 
 
-
     }
 
-    public static ArrayList<Integer> FEs(ArrayList<Integer> absences,int TimesMetAWeek){
-        ArrayList<Integer>list = new ArrayList<Integer>();
-        for (int i = 0; i < absences.size() ; i++) {
-            if(absences.get(i) <= (2*TimesMetAWeek)){
+    public static ArrayList<Integer> FEs(ArrayList<Integer> absences, int TimesMetAWeek) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < absences.size(); i++) {
+            if (absences.get(i) <= (2 * TimesMetAWeek)) {
                 list.add(i);
 
             }
@@ -137,12 +195,14 @@ public class Main {
         }
         return list;
     }
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
 
